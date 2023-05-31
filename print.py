d3bb90349b0330c989383ca312b8cd066ac75ada
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-05-30 21:53:51 -0500

    print lecture completed

---
+++ print.py
@@ -0,0 +1,25 @@
+example_1 = 3.1415
+example_2 = 24601
+example_3 = True
+
+print(example_1)
+print(example_2)
+print(example_3)
+
+example_5 = 10 + 9
+print(example_5)
+
+print((4 + 5) * 3)
+
+# print() exercises
+# Do all of this in a .py file in Pycharm.  Make a comment for each line of code you write.
+
+# create a variable, assign it a value of any data type, then use print() to display its assigned value in the output
+hello = "Hello World"
+print(hello)
+
+# use print() to print a value of any data type directly
+print("Hello World")
+
+# use print() to display the result of an expression that uses at least 2 math operators
+print(5 + 5 * 2)
