8bbc4d93f2b712040a035886c4bf1998517966e5
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-05-30 22:55:12 -0500

    lesson completed

---
+++ strings.py
@@ -0,0 +1,13 @@
+ex_1 = "This is a string"
+ex_2 = "This is also a string"
+
+ex_8 = "orange"
+print(ex_8[0] + ex_8[1] + ex_8[2] + ex_8[3] + ex_8[4] + ex_8[5])
+
+array = ["1", "2", "3", "4", "5"]
+print(array[0], array[1], array[2], array[3], array[4])
+
+tomato = "tomato"
+print(tomato[0:3])
+print(tomato[:3])
+print(tomato[2:])
